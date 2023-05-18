b7075d9e9fca679553a1f06d34e2d5b3c001bbbf
Abdelwakil RABIDI <abdelwakil.rabidi@beyn.io>
2023-05-18 01:36:22 +0100

    login service and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'contact-manager-front';
+export class AppComponent implements OnInit{
+  title = 'contact-manager';
+  sideBarOpen = true;
+  isLoggedIn = false;
+  userFullName="";
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+  constructor(private router: Router) {
+    router.events.subscribe((val)=>{
+      this.isLoggedIn = !!localStorage.getItem('token');
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.isLoggedIn) {
+      let res
+      let stringData = localStorage.getItem('authUser');
+      if (stringData) {
+        console.log(stringData);
+        res = JSON.parse(stringData);
+        this.userFullName = res.user.firstName +" "+res.user.lastName;
+      }
+    }
+  } 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,104 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CompanyComponent } from './company/company.component';
+import { ContactComponent } from './contact/contact.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { LogoutComponent } from './logout/logout.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AddEditUserComponent } from './dialogs/add-edit-user/add-edit-user.component';
+import { AddEditProfileComponent } from './dialogs/add-edit-profile/add-edit-profile.component';
+import { AddEditCompanyComponent } from './dialogs/add-edit-company/add-edit-company.component';
+import { AddEditContactComponent } from './dialogs/add-edit-contact/add-edit-contact.component';
+import { JwtInterceptor } from './helpers/JwtInterceptor';
+import { ChangePassComponent } from './dialogs/change-pass/change-pass.component';
+import { ShowPrivilegeComponent } from './dialogs/show-privilege/show-privilege.component';
+import { ShowContactsComponent } from './dialogs/show-contacts/show-contacts.component';
+import { ShowCompaniesComponent } from './dialogs/show-companies/show-companies.component';
+import { AddContactToCompanyComponent } from './dialogs/add-contact-to-company/add-contact-to-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidenavComponent,
+    UserComponent,
+    ProfileComponent,
+    CompanyComponent,
+    ContactComponent,
+    LogoutComponent,
+    AddEditUserComponent,
+    AddEditProfileComponent,
+    AddEditCompanyComponent,
+    AddEditContactComponent,
+    ChangePassComponent,
+    ShowPrivilegeComponent,
+    ShowContactsComponent,
+    ShowCompaniesComponent,
+    AddContactToCompanyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true,
+  },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
